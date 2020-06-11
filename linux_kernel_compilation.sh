3acec59caf66ac5660d0d8abf6e0f9353b78eb44
Kevin Chevreuil - Kaisen <kaisen@kaisen-linux.org>
2020-06-11 06:07:12 +0200

    Remove libqt4-dev, replace apt-get to apt, concatenation apt update and upgrade

--- linux_kernel_compilation.sh
+++ linux_kernel_compilation.sh
@@ -5,12 +5,10 @@ if [[ "$EUID" -ne 0 ]]; then
 	echo -e "Run this script as root"
 	exit 1
 fi
-#Update repositories
-apt-get update
-#Upgrade packages
-apt-get -y full-upgrade
+#Update and upgrade repositories
+apt update;apt -y full-upgrade
 #Dependancies install for compile kernel sources (dependancies compatibles for kernel 4.x)
-apt-get install debconf-utils dpkg-dev debhelper build-essential libncurses5-dev libelf-dev liblz4-tool bc libssl-dev xz-utils ncurses-dev git initramfs-tools dpkg-dev bin86 libglib2.0-dev libgtk2.0-dev libglade2-dev libqt4-dev pkg-config bison flex po-debconf xmlto gettext wget rsync -y
+apt install debconf-utils dpkg-dev debhelper build-essential libncurses5-dev libelf-dev liblz4-tool bc libssl-dev xz-utils libncurses-dev git initramfs-tools bin86 libglib2.0-dev libgtk2.0-dev libglade2-dev pkg-config bison flex po-debconf xmlto gettext wget rsync -y
 #Move to /usr/src directory for work
 cd /usr/src
 #Download kernel 5.6.19 on official source
@@ -21,10 +19,10 @@ tar xvf linux-5.6.19.tar.xz
 rm linux-5.6.19.tar.xz
 #Move to folder linux-5.6.19 obtain for extract tar archive
 cd linux-5.6.19/
-#Elements a compile custom kernel
+#Kernel personnalization
 make menuconfig
 #Build and compile custom kernel and create entries for utilisation and deb packages creates
-#kaisenlinux can be replace by your choice name of your custom kernel
+#customkernel can be replace by your choice name of your custom kernel
 make deb-pkg -j"$(nproc)" LOCALVERSION=-customkernel KDEB_PKGVERSION="$(make kernelversion)-1"
 #Move parent directory
 cd ..
